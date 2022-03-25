85484894c6b7320423676cc221e29997bb1b9b42
Vheekey <vicformidable@gmail.com>
2022-03-25 12:17:47 +0100

    wrote user seeder and created run setup command for migration and seeding

---
+++ app/Console/Commands/RunSetup.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class RunSetup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'run:setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup Initial database records';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if(! env('DB_DATABASE')){
+            $this->info('Database key absent!');
+
+            return;
+        }
+
+        $this->info('Running Migration...');
+        Artisan::call('migrate');
+        $this->info('Migration Completed!');
+
+        $this->info('Creating Users...');
+        Artisan::call('db:seed');
+        $this->info('Users Seeded!');
+
+        $this->info('Setup Complete!');
+    }
+}
